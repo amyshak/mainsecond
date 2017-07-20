c2e737ae1d8ad7ff02fd1a5254a979be43ee4716
amyshak <alexxmyshak@gmail.com>
2017-07-20 20:35:15 +0300

    first commit0720

---
+++ src/MyFirstClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by user on 20.07.2017.
+ */
+public class MyFirstClass {
+
+
+
+    public static void main(String[] arg){
+        System.out.print("Hello world");
+
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+    }
+}
