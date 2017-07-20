3d8b27b3e43f25fcb1af133b73c653ffb6dc1ceb
amyshak <alexxmyshak@gmail.com>
2017-07-20 21:48:01 +0300

    commit.message07202

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -13,5 +13,22 @@ public class MyFirstClass {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
         System.out.println(line);
+
+
+
+        int e = 6;
+        float f = 1;
+
+        boolean boolExpr = e < f;
+
+        if (boolExpr) {
+            System.out.println("true");
+        } else {
+
+            System.out.println("false");
+        }
+
+
+
     }
 }
