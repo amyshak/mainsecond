1744d0d101840cc3e051f13e20297a5dc36c3d5f
amyshak <alexxmyshak@gmail.com>
2017-07-30 23:22:38 +0300

    homework 0730

---
+++ src/com/mainacademy/homework/FirstHomeWork.java
@@ -0,0 +1,27 @@
+package com.mainacademy.homework;
+
+/**
+ * Created by user on 30.07.2017.
+ */
+public class FirstHomeWork {
+    public static void main(String[] args)
+    {
+        String name = "In cats we trust";
+        System.out.print(name);
+    }
+    // Вывод на экран переменной name
+
+
+    {
+        int a;
+
+    }
+    }
+
+
+
+
+
+
+
+}
