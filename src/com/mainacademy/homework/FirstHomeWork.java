89c34f156ac66b8cd108f50b2b6efd01e26049a9
amyshak <alexxmyshak@gmail.com>
2017-08-07 22:47:30 +0300

    домашнее задание 2 задачи 08/07/17

--- src/com/mainacademy/homework/FirstHomeWork.java
+++ src/com/mainacademy/homework/FirstHomeWork.java
@@ -1,19 +1,47 @@
 package com.mainacademy.homework;
 
+import java.util.Scanner;
+
 /**
  * Created by user on 30.07.2017.
  */
 public class FirstHomeWork {
-    public static void main(String[] args)
-    {
-        String name = "In cats we trust";
-        System.out.print(name);
+    public static void main(String[] args) {
+        {
+            String name = "In cats we trust";
+            System.out.print(name);
+        }
+        // Вывод на экран переменной name
+
+
+        {
+            int a;
+            Scanner scNumber = new Scanner(System.in);
+            System.out.println("Введите число");
+            a = scNumber.nextInt();
+            System.out.print("Квадрат числа=" + a * a);
+        }
+        //  Написать программу, которая считывает с клавиатуры целое число a и выводит квадрат этого числа
+
+
+        public class Solution {
+            public static int e = 1;
+            public static int b = -3;
+            public static int c = 9;
+            public static int d = -27;
+
+            int result = +e + b + c + d;
+        System.out.println(result);
+
+        }
     }
-    // Вывод на экран переменной name
+
+}
 
 
 
-    }
+
+
 
 
 
